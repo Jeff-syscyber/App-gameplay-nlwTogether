1652b4c3f6403290b0c0612d2d5221e3c3d090a1
Jeff-syscyber <vamolol43@gmail.com>
2021-08-23 03:08:07 -0300

    Navigation and Components

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,45 +1,47 @@
 import React from "react";
 import {
-    View,
-    Text,
-    Image,
-    StatusBar,
+  View,
+  Text,
+  Image,
 } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
 
-import { ButtonIcon } from "../../components/Buttonicon";
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from "./styles";
 
+import { ButtonIcon } from "../../components/Buttonicon";
+
 export function SignIn() {
+  const navigation = useNavigation();
 
-    return (
-        <View style={styles.container}>
-            <StatusBar
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent
-            />
-            <Image
-                source={IllustrationImg}
-                style={styles.image}
-                resizeMode="stretch"
-            />
 
-<View style={styles.content}>
-    <Text style={styles.title} >
-        Organize{`\n`}suas jogatinas{`\n`}facilmente
-    </Text>
+  function handleSingIn() {
+    navigation.navigate('Home');
+  }
 
-    <Text style={styles.subtitle}>
-        Crie grupos para jogar seus games{`\n`}
-        favoritos com seus amigos
-    </Text>
+  return (
+    <View style={styles.container}>
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
 
-    <ButtonIcon
-        title="Entrar com Discord"
-        activeOpacity={0.8}
-    />
-</View>
-        </View>
-    );
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se{'\n'}e organize suas{'\n'}jogatinas
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games{'\n'}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon
+          title="Entrar com Discord"
+          onPress={handleSingIn}
+        />
+      </View>
+    </View>
+  );
 }
